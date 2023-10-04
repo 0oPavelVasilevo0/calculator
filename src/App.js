c7f24052117c33189bcafc78b7def158e19555c7
Pavel Vasilev <karterpahan@gmail.com>
2023-10-04 21:59:38 +0300

    fix styles

--- src/App.js
+++ src/App.js
@@ -3,41 +3,41 @@ import { MDBBtn, MDBCard, MDBCardBody, MDBCol, MDBContainer, MDBInput, MDBRow } 
 
 function App() {
   return (
-    < MDBContainer  id="drum-machine" className=' d-flex align-items-center justify-content-center ' style={{ height: '100vh' }} >
-      <MDBContainer  className='square border bg-dark border-2 border-dark rounded-1 d-flex justify-content-center align-items-center p-0' style={{ maxWidth: '18rem' }}>
+    < MDBContainer fluid id="drum-machine" className=' d-flex align-items-center justify-content-center px-0 bg-secondary' style={{ height: '100vh' }} >
+      <MDBContainer  className='bg-dark border-2  rounded-1 d-flex justify-content-center align-items-center p-0' style={{ maxWidth: '18rem' }}>
        
         <MDBCard shadow='0' className='p-1 bg-dark rounded-1' >
           {/* <MDBInput className='num__input '/> */}
-          <MDBContainer fluid className='square border bg-dark d-flex align-items-center justify-content-end text-light rounded-1' style={{height: '4rem', width: '100%'}}>123234</MDBContainer>
+          <MDBContainer fluid className=' bg-secondary bg-opacity-25 bg-gradient  d-flex align-items-center justify-content-end text-light rounded-1 mt-1' style={{height: '4rem', width: '97%'}}>123234</MDBContainer>
         <MDBCardBody className=' d-flex align-items-center justify-content-center pt-2 pb-1'>
           <MDBRow className='row-cols-1 row-cols-md-1 g-1'>
             <MDBCol className='d-flex  justify-content-center'>
-              <MDBBtn size='lg' color='info' className='norm__button'>7</MDBBtn>
-              <MDBBtn size='lg' color='info' className='norm__button mx-1'>8</MDBBtn>
-              <MDBBtn size='lg' color='info' className='norm__button me-1'>9</MDBBtn>
-              <MDBBtn size='lg' color='warning' className='norm__button'>/</MDBBtn>
+              <MDBBtn size='lg' className='norm__button bg-info bg-opacity-50 shadow-0'>7</MDBBtn>
+              <MDBBtn size='lg'  className='norm__button mx-1 bg-info bg-opacity-50 shadow-0'>8</MDBBtn>
+              <MDBBtn size='lg'  className='norm__button me-1 bg-info bg-opacity-50 shadow-0'>9</MDBBtn>
+              <MDBBtn size='lg' className='norm__button bg-warning bg-opacity-50 shadow-0'>/</MDBBtn>
               </MDBCol>
               <MDBCol className='d-flex  justify-content-center'>
-                <MDBBtn size='lg' color='info' className='norm__button'>4</MDBBtn>
-                <MDBBtn size='lg' color='info' className='norm__button mx-1'>5</MDBBtn>
-                <MDBBtn size='lg' color='info' className='norm__button me-1'>6</MDBBtn>
-                <MDBBtn size='lg' color='warning' className='norm__button'>*</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button bg-info bg-opacity-50' shadow-0>4</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button mx-1 bg-info bg-opacity-50 shadow-0'>5</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button me-1 bg-info bg-opacity-50 shadow-0'>6</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button bg-warning bg-opacity-50 shadow-0'>*</MDBBtn>
               </MDBCol>
               <MDBCol className='d-flex  justify-content-center'>
-                <MDBBtn size='lg' color='info' className='norm__button'>1</MDBBtn>
-                <MDBBtn size='lg' color='info' className='norm__button mx-1'>2</MDBBtn>
-                <MDBBtn size='lg' color='info' className='norm__button me-1'>3</MDBBtn>
-                <MDBBtn size='lg' color='warning' className='norm__button' >-</MDBBtn>
+                <MDBBtn size='lg' className='norm__button bg-info bg-opacity-50 shadow-0'>1</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button mx-1 bg-info bg-opacity-50 shadow-0'>2</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button me-1 bg-info bg-opacity-50 shadow-0'>3</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button bg-warning bg-opacity-50 shadow-0' >-</MDBBtn>
               </MDBCol>
               <MDBCol className='d-flex  justify-content-center'>
-                <MDBBtn size='lg' color='info' className='big__button'>0</MDBBtn>
-                <MDBBtn size='lg' color='info' className='norm__button mx-1'>.</MDBBtn>
+                <MDBBtn size='lg'  className='big__button bg-info bg-opacity-50 shadow-0'>0</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button mx-1 bg-info bg-opacity-50 shadow-0'>.</MDBBtn>
               
-                <MDBBtn size='lg' color='warning' className='norm__button'>+</MDBBtn>
+                <MDBBtn size='lg'  className='norm__button bg-warning bg-opacity-50 shadow-0'>+</MDBBtn>
               </MDBCol>
               <MDBCol className='d-flex  justify-content-center'>
-                <MDBBtn size='lg' color='danger' className='big__button me-1'>AC</MDBBtn>
-                <MDBBtn size='lg' color='success' className='big__button '>=</MDBBtn>
+                <MDBBtn size='lg' className='big__button me-1 bg-danger bg-opacity-50 shadow-0'>AC</MDBBtn>
+                <MDBBtn size='lg'  className='big__button bg-success bg-opacity-50 shadow-0'>=</MDBBtn>
               
               </MDBCol>
               </MDBRow>
